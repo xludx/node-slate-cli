8671c3ab3b8a254a8f506d9f7fc4cba15eb11345
Juha Kovanen <juha.kovanen@gmail.com>
2019-09-04 13:28:04 +0300

    optional rebuild

--- cmds/serve.js
+++ cmds/serve.js
@@ -1,21 +1,27 @@
 const liveServer = require('live-server');
 const chokidar = require('chokidar');
-const build = require('./build');
 
 module.exports = (args) => {
-	build(args);
 	const port = args._[1] ? args._[1] : 4567;
-	const root = args._[2] ? args._[2] : './docs';
+    const root = args._[2] ? args._[2] : './docs';
+    const rebuild = args._[3] ? args._[3] : true;
 
-	const watcher = chokidar.watch(['./slate-config.yml', './src/docs']);
-	watcher.on('ready', () => console.log('Initial scan complete. Ready for changes!'))
-		.on('all', (event, path) => {
-			build(args);
-		});
+    if (rebuild === true) {
+        const build = require('./build');
+        build(args);
+
+        const watcher = chokidar.watch(['./slate-config.yml', './src/docs']);
+        watcher.on('ready', () => console.log('Initial scan complete. Ready for changes!'))
+            .on('all', (event, path) => {
+                build(args);
+            });
+    }
 
 	liveServer.start({
 		port: port,
 		root: root,
 		open: true
 	});
+
+    console.log('docs liveServer started!')
 };
