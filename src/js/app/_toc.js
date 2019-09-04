c7720cf2a426f90f7162715e2b4272173a06415a
Juha Kovanen <juha.kovanen@gmail.com>
2019-09-04 18:10:25 +0300

    3 level TOC

--- src/js/app/_toc.js
+++ src/js/app/_toc.js
@@ -3,7 +3,7 @@
    'use strict';
    function makeToc() {
       var options = {
-         selectors:       'h1, h2',
+         selectors:       'h1, h2, h3',
          extendPage:      false,
          theme:           'none',
          smoothScroll:    false,
